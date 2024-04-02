7e8c7858ea1f2bc953a61afce1738786575b6249
akbl-student <160585930+akbl-student@users.noreply.github.com>
2024-04-02 11:20:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+var messag = "hello ali";
+console.log(messag);
+var email = "ali@gmail.com";
+var pass = "123456";
+if (email == "ali@gmail.com" && pass == "123456") {
+    console.log("successfully logged in");
+}
+else {
+    console.log("invalid creds");
+}
+var percentage = 69.9;
+if (percentage >= 90) {
+    console.log("A+ Grade");
+}
+else if (percentage >= 80 && percentage <= 89.99) {
+    console.log("A Grad");
+}
+else if (percentage >= 70 && percentage <= 79.99) {
+    console.log("B Grad");
+}
+else if (percentage >= 60 && percentage <= 69.99) {
+    console.log("C Grad");
+}
+//if(email == "ali@gmail.coim" && pass == "123456"){console.log("successfully logged in");}
+//else{console.log("invalid creds")}
+var email = "ali@gmail.com";
+var pass = "123456";
+console.log(messag);
+if (email == "ali@gmail.com" && pass == "123456") {
+    console.log("successfully logged in");
+}
+else {
+    console.log("invalid creds");
+}
+var percentage = 69.9;
+if (percentage >= 90) {
+    console.log("A Grad");
+}
+else
+    (percentage >= 80 && percentage <= 89.99);
+{
+    console.log("A Grad");
+}
---
+++ app.ts
@@ -0,0 +1,37 @@
+let messag = "hello ali";
+console.log (messag);
+let email:string = "ali@gmail.com"
+let pass:string = "123456"
+
+if(email == "ali@gmail.com" && pass == "123456"){console.log("successfully logged in");}
+else{console.log("invalid creds")}
+let percentage = 69.9
+
+if (percentage >= 90){
+    console.log("A+ Grade");
+
+}
+else if ( percentage >= 80 && percentage <= 89.99){
+    console.log("A Grad");
+}
+else if (percentage >= 70 && percentage <= 79.99){
+    console.log("B Grad");
+}
+else if (percentage >= 60 && percentage <= 69.99){
+    console.log("C Grad");
+}
+//if(email == "ali@gmail.coim" && pass == "123456"){console.log("successfully logged in");}
+//else{console.log("invalid creds")}
+let email:string = "ali@gmail.com"
+let pass:string = "123456"
+console.log (messag);
+if (email == "ali@gmail.com" && pass == "123456"){console.log("successfully logged in");}
+else{console.log("invalid creds")}
+let percentage = 69.9
+if (percentage >= 90){
+    console.log("A Grad");
+}
+else (percentage >= 80 && percentage <= 89.99){
+    console.log("A Grad");
+}
+
